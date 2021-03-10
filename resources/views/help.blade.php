71ff6286b9ece22654951b63eb901831db2e8578
mirco verderosa <verderosamircowork@gmail.com>
2021-03-10 16:51:12 +0100

    Laravel primi passi

---
+++ resources/views/help.blade.php
@@ -0,0 +1,6 @@
+<html>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, consectetur dolorem! Earum accusantium exercitationem unde. Quidem deleniti dolor inventore accusamus quia nulla. Nam dignissimos hic repellendus a quae deserunt praesentium.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit aspernatur temporibus totam eos rerum quasi ut, iure voluptas placeat a non neque esse dolores nam ullam odio, mollitia atque impedit.
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta minima deleniti quo tenetur tempore illum ab a sequi quam quibusdam dolores, aspernatur autem cumque unde temporibus! Repellat eius expedita iure!</p></p>
+
+</html>
